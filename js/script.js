df29344cfaedced9219805c4f1e65682cb088d88
genfengl <genfeng.liu@gmail.com>
2022-09-19 14:37:20 +0800

    game procedure complete 2 players can take turn playing x and o now

--- js/script.js
+++ js/script.js
@@ -1,29 +1,49 @@
 const board = document.querySelector('#board')
 console.log(board)
-const message = document.querySelector('.message')
-
+const messageDiv = document.querySelector('.message')
+const message = messageDiv.children[0]
+let currentPlayer = ''
+let gameFinished = false
 
 const startGame = () => {
     const whoGoesFirst = Math.random()
-
     if (whoGoesFirst < 0.5) {
+        currentPlayer = 'Player 1'
         board.className = 'board x'
-        const newMessage = document.createElement('p')
-        newMessage.textContent = "Player 1's turn!"
-        message.appendChild(newMessage)
     } else {
+        currentPlayer = 'Player 2'
         board.className = 'board o'
-        const newMessage = document.createElement('p')
-        newMessage.textContent = "Player 2's turn!"
-        message.appendChild(newMessage)
     }
+}
 
+const switchSide = () => {
+    if (currentPlayer === 'Player 1') {
+        currentPlayer = 'Player 2'
+        board.className = 'board o' 
+        message.textContent = `${currentPlayer}'s turn!`
+    } else if (currentPlayer === 'Player 2') {
+        currentPlayer = 'Player 1'
+        board.className = 'board x'
+        message.textContent = `${currentPlayer}'s turn!`
+    }
+}
+
+const playGame = () => {
+    message.textContent = `${currentPlayer}'s turn`
+    board.addEventListener('click', (ev) => {
+        if (ev.target.className === 'cell') {
+            if (currentPlayer === 'Player 1') {
+                ev.target.className += ' x' 
+                switchSide()
+            } else if (currentPlayer === 'Player 2') {
+                ev.target.className += ' o'
+                switchSide()
+            }
+        } 
+    })
 }
 
 startGame()
+playGame()
 
-// board.addEventListener('click', (event) => {
-//     if (event.target.className === 'cell') {
-//         event.target.className += ' x'
-//     }
-// })
+
