145680fb1e5477eaf6c5722d81ccb68afcb74154
genfengl <genfeng.liu@gmail.com>
2022-09-19 13:46:44 +0800

    start game: setting up who starts first and display a message to show whose turn it is

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-const board = document.querySelector('.board')
+const board = document.querySelector('#board')
+console.log(board)
+const message = document.querySelector('.message')
 
 
+const startGame = () => {
+    const whoGoesFirst = Math.random()
+
+    if (whoGoesFirst < 0.5) {
+        board.className = 'board x'
+        const newMessage = document.createElement('p')
+        newMessage.textContent = "Player 1's turn!"
+        message.appendChild(newMessage)
+    } else {
+        board.className = 'board o'
+        const newMessage = document.createElement('p')
+        newMessage.textContent = "Player 2's turn!"
+        message.appendChild(newMessage)
+    }
+
+}
+
+startGame()
 
 // board.addEventListener('click', (event) => {
 //     if (event.target.className === 'cell') {
-//         const newDiv = document.createElement('div')
-//         event.target.style.backgroundColor = 'red'
+//         event.target.className += ' x'
 //     }
 // })
