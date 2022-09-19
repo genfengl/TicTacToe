b41856e8d4042b9d95ac96b8ea9932b76bb95f78
genfengl <genfeng.liu@gmail.com>
2022-09-19 12:27:31 +0800

    layout framekwork complete

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const board = document.querySelector('.board')
+
+
+
+// board.addEventListener('click', (event) => {
+//     if (event.target.className === 'cell') {
+//         const newDiv = document.createElement('div')
+//         event.target.style.backgroundColor = 'red'
+//     }
+// })
